12d3b89811512abadf11ccff4cded3e5f0364dfb
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-03-11 16:40:57 +0100

    Beautified entire code

--- src/Accounts.tsx
+++ src/Accounts.tsx
@@ -16,87 +16,88 @@ import Token from './Token'
 import { Rowing } from '@material-ui/icons';
 
 interface Account {
-    id: number,
-    user: string,
-    surname_name: string,
-    password: string,
+  id: number,
+  user: string,
+  surname_name: string,
+  password: string,
 }
 
 class AccountsForm extends Component {
 
-    rows : Array<JSX.Element> ;
+  rows: Array<JSX.Element>;
 
-    constructor(props) {
-        super(props)
-        this.rows = new Array()
-    }
+  constructor(props) {
+    super(props)
+    this.rows = new Array()
+  }
 
-    private addTableRow(row : Account) {
-        this.rows.push(
-            <TableRow key = {row.id}>
-                <TableCell component="th" scope="row" align="left">
-                    {row.id}
-                </TableCell>
+  private addTableRow(row: Account) {
+    this.rows.push(
+      <TableRow key={row.id}>
+        <TableCell component="th" scope="row" align="left">
+          {row.id}
+        </TableCell>
 
-                <TableCell align="center">{row.user}</TableCell>
-                <TableCell align="center">{row.surname_name}</TableCell>
-                <TableCell align="center">{row.password}</TableCell>
-            </TableRow>
-        )
-    }
+        <TableCell align="center">{row.user}</TableCell>
+        <TableCell align="center">{row.surname_name}</TableCell>
+        <TableCell align="center">{row.password}</TableCell>
+      </TableRow>
+    )
+  }
 
-    private retrieveAccounts() {
-        const config = { headers: {"Authorization": Token.get()}};
-        axios.post("/api", {}, config).then((res) => {
-            for(var id in res.data) {
-                const data = res.data[id]
-                const newAccount = {
-                    id: data.id,
-                    user: data.user,
-                    surname_name: data.surname_name,
-                    password: data.password
-                };
-                this.addTableRow(newAccount);
-            }
-            this.forceUpdate()
-        })
-    }
+  private retrieveAccounts() {
+    const config = { headers: { "Authorization": Token.get() } };
+    axios.post("/api", {}, config).then((res) => {
+      for (var id in res.data) {
+        const data = res.data[id]
+        const newAccount = {
+          id: data.id,
+          user: data.user,
+          surname_name: data.surname_name,
+          password: data.password
+        };
+        this.addTableRow(newAccount);
+      }
+      this.forceUpdate()
+    })
+  }
 
-    componentDidMount() {
-        this.retrieveAccounts()
-    }
+  componentDidMount() {
+    this.retrieveAccounts()
+  }
 
-    render() {
-        return (
-            <div>
-                <TableContainer component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead className="headerCard">
-                            <TableRow>
-                                <TableCell align="center">Username</TableCell>
-                                <TableCell align="center">Surname - Name</TableCell>
-                                <TableCell align="center">Password</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>{this.rows}</TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-        )};
+  render() {
+    return (
+      <div>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead className="headerCard">
+              <TableRow>
+                <TableCell align="center">Username</TableCell>
+                <TableCell align="center">Surname - Name</TableCell>
+                <TableCell align="center">Password</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{this.rows}</TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    )
+  };
 }
 
 const Accounts = () => {
 
-    if (!Token.get())
-        location.href = "/login"
+  if (!Token.get())
+    location.href = "/login"
 
-    return (
-        GeneralLayout(
-            <div>
-                <AccountsForm />
-            </div>
-        )
-    );
+  return (
+    GeneralLayout(
+      <div>
+        <AccountsForm />
+      </div>
+    )
+  );
 }
 
 export default Accounts;
