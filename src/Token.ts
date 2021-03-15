e223b63a7e3d36b27f4f44c6ff473f34c0d15ac8
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-03-15 22:46:07 +0100

    Renamed TOKEN_AUTH -> token_id

--- src/Token.ts
+++ src/Token.ts
@@ -1,15 +1,15 @@
 class Token {
 
     public static get() : string | null {
-        return localStorage.getItem("TOKEN_AUTH");
+        return localStorage.getItem("token_id");
     }
 
     public static set(token : string) : void {
-        localStorage.setItem("TOKEN_AUTH", token);
+        localStorage.setItem("token_id", token);
     }
 
     public static remove() : void {
-        localStorage.removeItem("TOKEN_AUTH");
+        localStorage.removeItem("token_id");
     }
 }
 
