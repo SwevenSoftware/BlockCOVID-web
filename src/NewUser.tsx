c1936e7160adbbe46e836e98c25d6c51443ba761
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-03-21 11:32:38 +0100

    Edited comment in if flagErr condition

--- src/NewUser.tsx
+++ src/NewUser.tsx
@@ -162,7 +162,7 @@ export default function FormDialog() {
     flagErr = (passInputControl(pass) ? true : flagErr);
     flagErr = (passConfirmInputControl(pass, passConfirm) ? true : flagErr);
 
-    if(!flagErr) { /* if no error has occured */
+    if(!flagErr) { /* no error has occured */
       const config = {
         headers: {
           "Content-Type": "application/json",
