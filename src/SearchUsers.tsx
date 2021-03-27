c1106029b17f3a07ca23e5f98c892c859eef0dcc
Michele <michelemotterle97@gmail.com>
2021-03-27 20:05:13 +0100

    Counter uploaded

--- src/SearchUsers.tsx
+++ src/SearchUsers.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import AddBoxIcon from '@material-ui/icons/AddBox';
-import { IconButton } from '@material-ui/core';
+import { IconButton, ListItemIcon, ListItem } from '@material-ui/core';
 
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import {theme} from './theme';
+import PeopleIcon from '@material-ui/icons/People';
+import SecurityIcon from '@material-ui/icons/Security';
+import WorkIcon from '@material-ui/icons/Work';
+import BathtubIcon from '@material-ui/icons/Bathtub';
 
 const SearchUsers = () => {
     const [isModalOpen_A, setModalState] = React.useState(false);
@@ -14,23 +18,16 @@ const SearchUsers = () => {
     return (
       <ThemeProvider theme={theme}>    
         <div className="search">
-            <Autocomplete
-              id="searchUsers"
-              freeSolo
-              options={usersList.map((option) => option.name)}
-              renderInput={(params) => (
-                  <TextField {...params} label="Cerca utente" variant="outlined" />
-              )}
-            />
+            <ListItem>
+              <h4 className="counter">Account:</h4>
+              <ListItemIcon className="spacing"><PeopleIcon className="people"/><p className="number">34</p></ListItemIcon> 
+              <ListItemIcon className="spacing"><SecurityIcon className="shield"/><p className="number">6</p> </ListItemIcon>
+              <ListItemIcon className="spacing"><WorkIcon className="bag"/><p className="number">8</p> </ListItemIcon>
+              <ListItemIcon className="spacing"><BathtubIcon className="cleaner"/><p className="number">10</p></ListItemIcon>
+            </ListItem>
         </div>
       </ThemeProvider>
     )
 }
-
-const usersList = [
-    { name: 'Gino' },
-    { name: 'Silvio' },
-    { name: 'Abelarda' },
-];
 
 export default SearchUsers;
