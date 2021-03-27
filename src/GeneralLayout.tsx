97d39276ca2eedb2ef2327aeb93ffd20e86b6a6f
Dardan Kokollari <38751431+dkokollari@users.noreply.github.com>
2021-03-27 16:27:39 +0100

    Removed RefObject import

--- src/GeneralLayout.tsx
+++ src/GeneralLayout.tsx
@@ -1,4 +1,4 @@
-import React, {RefObject} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
