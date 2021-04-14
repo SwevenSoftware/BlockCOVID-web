77780c1e7041a97ff27c7652fdae0e80d3bcb35d
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-04-14 17:52:10 +0200

    Added types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const LOGIN_FAILURE = "LOGIN_FAILURE"
+export const LOGOUT = "LOGOUT"
+
+/* login */
+export const ERROR_USER_NO_AUTH = "Accesso non autorizzato. Si prega di contattare l'amministratore"
+export const ERROR_USER_OR_PASS = "Username o password scorretta. Riprova"
+
+/* others */
+export const ERROR_UNKNOWN = "Errore sconosciuto. Si prega di contattare l'amministratore"
