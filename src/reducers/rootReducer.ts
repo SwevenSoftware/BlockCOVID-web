2543f2c97cb989ed152a9e36ad83771aee3eb1bd
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-04-14 17:53:49 +0200

    Removed not existing reducers

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,15 +1,9 @@
 import { combineReducers } from 'redux';
-import exampleReducer from './exampleReducer';
 import loginReducer from './loginReducer';
-import infoLoginReducer from './newLoginReducer';
-
 
 const rootReducer = combineReducers({
-   //counter: exampleReducer, // WARNING: read comment below
-   //logged: loginReducer,
-   newLogin: infoLoginReducer
+   login: loginReducer
 });
-// counter serves as a temporary example, if removed be cautious in ./App.tsx (connect)
 
 export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>
