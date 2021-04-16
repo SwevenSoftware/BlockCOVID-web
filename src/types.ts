4e2ba06957f2e7baaa8cc7179d8a9165ee8860ca
Michele <michelemotterle97@gmail.com>
2021-04-16 11:19:31 +0200

    Update types.ts
    Added error for pencil and trash components

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,26 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
 export const LOGIN_FAILURE = "LOGIN_FAILURE"
 export const LOGIN_LOGOUT = "LOGOUT"
 
+export const PENCIL_CONFIRM = "PENCIL_CONFIRM"
+export const PENCIL_CANCEL = "PENCIL_CANCEL"
+export const PENCIL_FAILURE = "PENCIL_FAILURE"
+export const PENCIL_SUCCESS = "PENCIL_SUCCESS"
+
+export const TRASH_CONFIRM = "TRASH_CONFIRM"
+export const TRASH_CANCEL = "TRASH_CANCEL"
+
 /* login */
 export const ERROR_USER_NO_AUTH = "Accesso non autorizzato. Si prega di contattare l'amministratore"
 export const ERROR_USER_OR_PASS = "Username o password scorretta. Riprova"
 
+/* pencil */
+export const ERROR_PASSWORD_VALID = "Password non valida"
+export const ERROR_PASSWORD_MATCH = "Le password inserite non corrispondono"
+export const ERROR_AUTHORITIES = "Si prega di scegliere almeno una opzione"
+
+/* trash */
+export const ERROR_USER_CANNOT_BE_DELETED = "Non puoi eliminare il tuo account"
+
 /* others */
 export const ERROR_UNKNOWN = "Errore sconosciuto. Si prega di contattare l'amministratore"
+export const ERROR_GENERIC_ERROR = "Si è verificato un errore"
