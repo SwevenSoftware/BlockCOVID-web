62c2fb0a42e9850afd5dd06f4edc5dbfb47200d9
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-04-17 16:24:10 +0200

    Added actions getAccounts, success, failure

--- src/actions/accountActions.ts
+++ src/actions/accountActions.ts
@@ -1,4 +1,24 @@
+import {
+  ACCOUNT_SUCCESS,
+  ACCOUNT_FAILURE
+} from "../types"
 
+export const getAccounts = () => {
+  return (dispatch, getState) => {
+    console.log('current state:', getState()) // WARNING: testing purposes
+  }
 }
 
+const success = (data) => ({
+  type: ACCOUNT_SUCCESS,
+  payload: {
+    ...data
+  }
+})
 
+const failure = (error) => ({
+  type: ACCOUNT_FAILURE,
+  payload: {
+    error
+  }
+})
