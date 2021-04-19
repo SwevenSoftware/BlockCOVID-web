b432eba2b238820e2be27485c3c7392cb2d456a3
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-04-19 15:20:49 +0200

    Implemented getAccounts(), renamed trashConfirm -> deleteAccount

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,30 @@
-import axios from 'axios';
-import Token from './Token'
+import axios from 'axios'
 
 export const login = ({username, password}) => {
   const config = {
     headers: { "Content-Type": "application/json"}
-  };
-
-  return axios.post("/api/login", JSON.stringify({username, password}), config);
+  }
+  return axios.post("/api/login", JSON.stringify({username, password}), config)
 }
 
-export const trashConfirm = ({username, link_delete}) => {
+export const getAccounts = (tokenID: string) => {
+  const config = {
+    data: {}, /* data must be set or else headers.Content-Type will be ignored */
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": tokenID
+    }
+  }
+  return axios.get("/api/admin/users", config)
+}
+
+export const deleteAccount = (username: string, link: string, tokenID: string) => {
   const config = {
     headers: {
       "Content-Type": "application/json",
-      "Authorization": Token.getId(),
+      "Authorization": tokenID,
       "username": username
     }
   }
-
-  return axios.delete(link_delete + username, config);
+  return axios.delete(link + username, config)
 }
