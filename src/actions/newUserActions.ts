8e233d56072282b1815020284474aed58626353d
Michele <michelemotterle97@gmail.com>
2021-04-21 16:43:55 +0200

    Create newUserActions.ts
    Created newUserConfirm()

---
+++ src/actions/newUserActions.ts
@@ -0,0 +1,9 @@
+import {
+
+} from "../types"
+
+export const newUserConfirm = ({username, password, confirmPassword}) => {
+   return (dispatch, getState) => {
+      //APIcall will be implemented
+   }
+}
