c987d89722dca6d33c25501dd7a93d6701603d0b
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-04-22 11:36:56 +0200

    Fixed syntax errors, beautified code

--- src/components/NewUserComponent.tsx
+++ src/components/NewUserComponent.tsx
@@ -1,134 +1,124 @@
 /* react */
-import React from 'react';
-import { Component } from 'react'
-
+import React, { Component } from 'react'
 /* redux */
 import { connect } from 'react-redux'
-import {newUserConfirm} from '../actions/newUserActions'
-
+import { newUserConfirm } from '../actions/newUserActions'
 /* material-ui */
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
-import AddBoxIcon from '@material-ui/icons/AddBox';
-import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { FormGroup, FormLabel, FormControl, withStyles, FormHelperText } from '@material-ui/core';
-
-/* notistack */
-import { VariantType, useSnackbar } from 'notistack';
-
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import IconButton from '@material-ui/core/IconButton'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { FormGroup, FormLabel, FormControl, withStyles, FormHelperText } from '@material-ui/core'
 /* styles */
-import { ThemeProvider } from '@material-ui/core/styles';
-import {theme} from '../theme';
-import './styles.css';
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from '../theme'
+import '../styles.css'
+/* others */
+import { VariantType, useSnackbar } from 'notistack'
 
 interface NewUserProps {
-   NewUserState: any,
-   NewUserDispatch: any
+  state: any,
+  dispatch: any
 }
 
 interface NewUserStates {
-   usernameValue: string,
-   passwordValue: string,
-   confirmPasswordValue: string,
-   isButtonDisabled: boolean,
-   isNewUserOpen: boolean,
-   authorities: any
+  usernameValue: string,
+  passwordValue: string,
+  confirmPasswordValue: string,
+  isButtonDisabled: boolean,
+  isNewUserOpen: boolean,
+  authorities: any
 }
 
 class NewUserComponent extends Component<NewUserProps, NewUserStates> {
-   constructor(props) {
-      super(props);
-      this.handleChangeAuthorities = this.handleChangeAuthorities.bind(this),
-      this.handleClickOpenButton = this.handleClickOpenButton.bind(this),
-      this.handleCloseButton = this.handleCloseButton.bind(this)
-      this.state = {
-         usernameValue: "",
-         passwordValue: "",
-         confirmPasswordValue: "",
-         isButtonDisabled: true,
-         isNewUserOpen: false,
-         authorities: {
-            checkedAdmin: false,
-            checkedUser: false,
-            checkedCleaner: false
-         }
+  constructor(props) {
+    super(props)
+    this.handleChangeAuthorities = this.handleChangeAuthorities.bind(this),
+    this.handleClickOpenButton = this.handleClickOpenButton.bind(this),
+    this.handleCloseButton = this.handleCloseButton.bind(this)
+    this.state = {
+      usernameValue: "",
+      passwordValue: "",
+      confirmPasswordValue: "",
+      isButtonDisabled: true,
+      isNewUserOpen: false,
+      authorities: {
+        checkedAdmin: false,
+        checkedUser: false,
+        checkedCleaner: false
       }
-   }
+    }
+  }
 
-   render() {
-      return(
-         <div></div>
-      )
-   }
+  render() {
+    return(
+      <div></div>
+    )
+  }
 
-   componentDidMount() {
-      this.setButton()
-   }
+  componentDidMount() {
+    this.setButton()
+  }
 
-   private setButton(
-      username: string = this.state.usernameValue, 
-      password: string = this.state.passwordValue, 
-      confirmPassword: string = this.state.confirmPasswordValue,
-      authorities: boolean[] = this.state.authorities): void {
-         if(username && password && confirmPassword && authorities){
-            this.setState({isButtonDisabled: false})
-         } else {
-            this.setState({isButtonDisabled: true})
-         }
-   }
+  private setButton(): void { // TODO: if condition
+    // if(username && password && confirmPassword && authorities) {
+    //   this.setState({isButtonDisabled: false})
+    // }
+    // else {
+    //   this.setState({isButtonDisabled: true})
+    // }
+  }
 
-   private handleChangeAuthorities() {
-      this.setState({...this.state, })
-   }
+  private handleChangeAuthorities(): void { // TODO: implementation
+    // this.setState({...this.state })
+  }
 
-   private handleClickOpenButton() {
-      this.setState({isButtonDisabled: true}) 
-   }
+  private handleClickOpenButton(): void {
+    this.setState({isButtonDisabled: true})
+  }
 
-   private handleCloseButton() {
-      this.setState({usernameValue: ""})
-      this.setState({passwordValue: ""})
-      this.setState({confirmPasswordValue: ""})
-      this.setState({isButtonDisabled: true})
-      this.setState({})
-      this.setState({
-         authorities: {
-            checkedAdmin: false,
-            checkedUser: false,
-            checkedCleaner: false
-         }
-      })
-
-      //not ended
+  private handleCloseButton() {
+    this.setState({usernameValue: ""})
+    this.setState({passwordValue: ""})
+    this.setState({confirmPasswordValue: ""})
+    this.setState({isButtonDisabled: true})
+    this.setState({
+      authorities: {
+        checkedAdmin: false,
+        checkedUser: false,
+        checkedCleaner: false
+      }
+    })
+    // TODO:
    }
 }
 
 const mapStateToProps = (state) => {
-   return {
-      state: {
-         newUserState: state.newUser
-      }
-   }
+  return {
+    state: {
+      newUser: state.newUser
+    }
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-   return {
-      dispatch: {
-         newUser: (data) => {
-            dispatch(newUserConfirm(data));
-         }
+  return {
+    dispatch: {
+      newUser: (data) => {
+        dispatch(newUserConfirm(data))
       }
-   }
+    }
+  }
 }
 
 export default connect(
-   mapStateToProps,
-   mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(NewUserComponent)
