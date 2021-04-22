bff2de9320832a4c8080314217e725f1451dccac
Michele <michelemotterle97@gmail.com>
2021-04-22 21:40:54 +0200

    implemented newUserConfirm, successMessage, failureMessage, created newUserCancel

--- src/actions/newUserActions.ts
+++ src/actions/newUserActions.ts
@@ -1,14 +1,43 @@
 import {
-
+   NEW_USER_CONFIRM,
+   NEW_USER_FAILURE,
+   NEW_USER_SUCCESS,
+   NEW_USER_CANCEL,
 } from "../types"
 
 import {createAccount} from "../api"
+
+export const newUserCancel = () => {
+   return {
+      type: NEW_USER_CANCEL,
+      payload: {
+         
+      }
+   }
+}
 
 export const newUserConfirm = ({tokenID, username, password, auth}) => {
    return (dispatch, getState) => {
       createAccount(tokenID, username, password, auth)
          .then((res) => {
-            dispatch()
+            dispatch(successMessage(res.data))
+         })
+         .catch(err => {
+            dispatch(failureMessage(err))
          })
    }
 }
+
+const successMessage = (data) => ({
+   type: NEW_USER_SUCCESS,
+   payload: {
+      ...data
+   }
+})
+
+const failureMessage = (error) => ({
+   type: NEW_USER_FAILURE,
+   payload: {
+      error
+   }
+})
