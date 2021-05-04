851058974f98ce6d2f1cf50a4704378a222e1881
Michele <michelemotterle97@gmail.com>
2021-05-04 16:21:40 +0200

    commented debug console.log

--- src/reducers/newUserReducer.ts
+++ src/reducers/newUserReducer.ts
@@ -11,13 +11,13 @@ const initialState = {
 export default function newUserReducer(state = initialState, action) {
    switch(action.type) {
       case NEW_USER_SUCCESS:
-         console.log("utente creato con successo")
+         //console.log("utente creato con successo")
          return {
             error: null
          }
       break;
       case NEW_USER_FAILURE:
-         console.log("impossibile creare utente")
+         //console.log("impossibile creare utente")
          switch(action.payload.error) {
             case 409: /* user exists, username already taken */
             return {
