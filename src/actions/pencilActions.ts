fc5117ef8d595165b6da969dd074f9dab6969f0d
Michele <michelemotterle97@gmail.com>
2021-05-09 17:11:51 +0200

    pencilConfirm action ended

--- src/actions/pencilActions.ts
+++ src/actions/pencilActions.ts
@@ -1,13 +1,35 @@
 import {
    PENCIL_SUCCESS,
-   PENCIL_FAILURE
+   PENCIL_FAILURE,
+   NEW_USER_SUCCESS
 } from "../types"
 
 import { modifyAccount } from '../api'
 import { getAccounts } from './accountsActions'
 
-export const pencilConfirm = ({tokenID, password, auth}) => {
-   return(dispatch) => {
-      
+export const pencilConfirm = ({tokenID, link, username, password, auth}) => {
+   return(dispatch, getState) => {
+      modifyAccount(tokenID, link, username, password, auth)
+         .then((res) => {
+            dispatch(successMessage(res.data))
+            dispatch(getAccounts(tokenID))
+         })
+         .catch (err => {
+            dispatch(failureMessage(err))
+         })
    }
 }
+
+const successMessage = (data) => ({
+   type: PENCIL_SUCCESS,
+   payload: {
+      ...data
+   }
+})
+
+const failureMessage = (error) => ({
+   type: PENCIL_FAILURE,
+   payload: {
+      error
+   }
+})
