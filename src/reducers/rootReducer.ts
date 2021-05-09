7457d76c37fe1b7cf3af800b1861ae913bc4e2e5
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-05-09 23:05:56 +0200

    Removed old and non-existing reducers

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux'
 import loginReducer from './loginReducer'
 import accountsReducer from './accountsReducer'
-import newUserReducer from './newUserReducer'
-import trashReducer from './trashReducer'
 
 const rootReducer = combineReducers({
     login: loginReducer,
-    accounts: accountsReducer,
-    newUser: newUserReducer,
-    trash: trashReducer,
+    accounts: accountsReducer
 })
 
 export default rootReducer
