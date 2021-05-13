5c52c553d6de5fd7ae84f5377073a66315c70150
FrancescoPecile <francesco.pecile.1@studenti.unipd.it>
2021-05-13 11:32:36 +0200

    loginaction test
    added testing dependencies and first test for async action

--- src/test/LoginActions.test.ts
+++ src/test/LoginActions.test.ts
@@ -1,44 +1,35 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-
-import * as actions from '../actions/loginActions'
-import * as types from '../types'
 import fetchMock from 'fetch-mock'
 import expect from 'expect'
-
-
 import React from 'react';
+import * as actions from '../actions/loginActions'
+import * as types from '../types'
+import configureStore from "../configureStore";
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
-it('It should render without errors', () => {
+describe('async actions', () => {
+    afterEach(() => {
+        fetchMock.restore()
+    })
 
+    it('dispatches a login action', () => {
+        fetchMock.getOnce('/api/account/login', {
+            body: {admin: 'admin', password: 'password'} ,
+            headers: { 'content-type': 'application/json' }
+        })
+
+        const expectedActions = [
+            { type: types.loginTypes.LOGIN_SUCCESS, payload: '1' }
+        ]
+        const store = mockStore({token:null,error:''})
+
+        return store.dispatch(actions.login({username: 'admin',password:'password'})).then(() => {
+            // return of async actions
+            expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
 })
-
-
-/*
- const middlewares = [thunk]
- const mockStore = configureMockStore(middlewares)
- describe ('async actions', () => {
-   afterEach(() =>  {
-      fetchMock.restore()
-   })
-
-   it ('creates LOGIN_SUCCESS when someone has insert the right credentials', () => {
-      fetchMock.getOnce('/login', {
-         body: {token: ['number']},
-         headers: {'content-type': 'application/json'}
-      })
-
-      const expectedApplications = [
-         { type: types.LOGIN_SUCCESS },
-         { type: types.LOGIN_FAILURE, body: { token: ['number'] } }
-      ]
-
-      const store = mockStore({ token: [] })
-
-      return store.dispatch(actions.login({username: 'admin', password: 'password'})).then(() => {
-         expect(store.getActions()).toEqual(expectedApplications)
-      })
-   })
-}) */
 
