53c83092bb5ae17c2e7642f0f54377ffc88bd72b
FrancescoPecile <francesco.pecile.1@studenti.unipd.it>
2021-05-16 14:29:49 +0200

    reducerTest

---
+++ src/test/Api/ReducerFactory.test.ts
@@ -0,0 +1,22 @@
+import {reducerFactory} from "../../reducers/reducerFactory";
+import {red} from "@material-ui/core/colors";
+
+describe('reducerFactory', () =>{
+    const handlers = {}
+    const action = {
+        type:'function'
+    }
+    handlers[action.type] = () =>({
+        value: 'value'
+    })
+    const initialState = {}
+
+    const reducer = reducerFactory( initialState, handlers)
+
+    it('non existent action returns state', () => {
+        expect(reducer(initialState,'wow')).toEqual(initialState)
+    })
+    it('existing action returns right state', () => {
+       expect(reducer(initialState,action)).toEqual({value:'value'})
+    })
+})
