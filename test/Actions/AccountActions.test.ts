b00b9af25c996c1abcc0ddc0c1709656367b3e02
Luca Zaninotto <lucazanny@gmail.com>
2021-05-17 16:39:42 +0200

    api testing

--- test/Actions/AccountActions.test.ts
+++ test/Actions/AccountActions.test.ts
@@ -1,10 +1,10 @@
 import accountsActions from "../../src/actions/accountsActions";
 import accountApi from '../../src/Api/accountAPI'
 
-jest.mock('accountApi')
+// jest.mock('accountApi')
 
 describe('Account Action', () => {
-    it('should ', function () {
+    it('should ', function() {
 
     });
 })
