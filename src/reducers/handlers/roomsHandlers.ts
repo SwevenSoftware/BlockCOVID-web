c6f8df27ba4acc99f6656eb30cb51d05c30ba6ee
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-05-19 23:59:17 +0200

    Added handlers for desk actions

--- src/reducers/handlers/roomsHandlers.ts
+++ src/reducers/handlers/roomsHandlers.ts
@@ -23,6 +23,14 @@ roomsHandlers[roomTypes.CREATE_SUCCESS] = function(state, action) {
     }
 }
 
+roomsHandlers[roomTypes.CREATE_DESKS_SUCCESS] = function(state, action) {
+    console.log(roomTypes.CREATE_DESKS_SUCCESS)
+    return {
+        ...state,
+        error: ""
+    }
+}
+
 roomsHandlers[roomTypes.MODIFY_SUCCESS] = function(state, action) {
     console.log(roomTypes.MODIFY_SUCCESS)
     return {
@@ -31,8 +39,29 @@ roomsHandlers[roomTypes.MODIFY_SUCCESS] = function(state, action) {
     }
 }
 
+roomsHandlers[roomTypes.MODIFY_DESK_SUCCESS] = function(state, action) {
+    console.log(roomTypes.MODIFY_DESK_SUCCESS)
+    return {
+        ...state,
+        error: ""
+    }
+}
+
 roomsHandlers[roomTypes.DELETE_SUCCESS] = function(state, action) {
     console.log(roomTypes.DELETE_SUCCESS)
+    if (action.payload.error) {
+        return {
+            ...state,
+            error: ""
+        }
+    }
+    else {
+        return state
+    }
+}
+
+roomsHandlers[roomTypes.DELETE_DESK_SUCCESS] = function(state, action) {
+    console.log(roomTypes.DELETE_DESK_SUCCESS)
     if (action.payload.error) {
         return {
             ...state,
@@ -68,6 +97,33 @@ roomsHandlers[roomTypes.CREATE_FAILURE] = function(state, action) {
     }
 }
 
+roomsHandlers[roomTypes.CREATE_DESKS_FAILURE] = function(state, action) {
+    console.log(roomTypes.CREATE_DESKS_FAILURE)
+    if(action.payload.error) {
+        switch (action.payload.error) {
+            case 400: /** bad desk position - may exceed room size */
+                return {
+                    ...state,
+                    error: ERROR_BAD_DESK_POSITION
+                }
+            case 409: /** desk already exists */
+                return {
+                    ...state,
+                    error: ERROR_DESK_ALREADY_EXISTS
+                }
+            default:
+                return {
+                    ...state,
+                    error: ERROR_UNKNOWN
+                }
+        }
+    }
+    else {
+        return state
+    }
+
+}
+
 roomsHandlers[roomTypes.MODIFY_FAILURE] = function(state, action) {
     console.log(roomTypes.MODIFY_FAILURE)
     if (action.payload.error) {
@@ -81,12 +137,51 @@ roomsHandlers[roomTypes.MODIFY_FAILURE] = function(state, action) {
     }
 }
 
+roomsHandlers[roomTypes.MODIFY_DESK_FAILURE] = function(state, action) {
+    console.log(roomTypes.MODIFY_DESK_FAILURE)
+    if (action.payload.error) {
+        console.log(action.payload.error)
+        switch (action.payload.error) {
+            default:
+                return {
+                    ...state,
+                    error: ERROR_UNKNOWN
+                }
+        }
+    }
+    else {
+        return state
+    }
+}
+
+
 roomsHandlers[roomTypes.DELETE_FAILURE] = function(state, action) {
     console.log(roomTypes.DELETE_FAILURE)
     if (action.payload.error) {
         return {
             ...state,
             error: ERROR_UNKNOWN,
+        }
+    }
+    else {
+        return state
+    }
+}
+
+roomsHandlers[roomTypes.DELETE_DESK_FAILURE] = function(state, action) {
+    console.log(roomTypes.DELETE_DESK_FAILURE)
+    if (action.payload.error) {
+        switch (action.payload.error) {
+            case 404: /** desk does not exist */
+                return {
+                    ...state,
+                    error: ERROR_DESK_DOES_NOT_EXIST
+                }
+            default:
+                return {
+                    ...state,
+                    error: ERROR_UNKNOWN
+                }
         }
     }
     else {
