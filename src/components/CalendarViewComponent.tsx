78a9b2b42c393478657af7150fe37d0ff18f19e2
Michele <michelemotterle97@gmail.com>
2021-05-25 11:39:38 +0200

    Create CalendarView, added import, states, props, Component class, default modal functions

---
+++ src/components/CalendarViewComponent.tsx
@@ -0,0 +1,80 @@
+/* react */
+import React, {Component} from 'react';
+/* redux */
+import { connect } from "react-redux"
+/* material-ui */
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+/* devexpress */
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  WeekView,
+  Toolbar,
+  DateNavigator,
+  Appointments,
+  TodayButton,
+} from '@devexpress/dx-react-scheduler-material-ui';
+/* styles */
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from '../theme'
+import { CalendarProps } from '@material-ui/pickers/views/Calendar/Calendar';
+
+interface CalendarViewProps {
+   state: any
+   dispatch: any
+   data: any
+}
+
+interface CalendarViewStates {
+   data: any
+   currentDate: Date,
+   isModalOpen: boolean
+}
+
+class CalendarViewComponent extends Component<CalendarViewProps, CalendarViewStates> {
+   constructor(props) {
+      super(props)
+      this.state = {
+         data: Appointments,
+         currentDate: new Date('2021-01-01T08:00'),
+         isModalOpen: false
+      }
+      this.handleClickOpen = this.handleClickOpen.bind(this)
+		this.handleClose = this.handleClose.bind(this)
+   }
+
+
+   
+
+   private currentDateChange(): void {
+      this.setState({currentDate: new Date()})
+   }
+
+   private handleClickOpen(): void {
+		this.setState({ isModalOpen: true })
+	}
+
+   private handleClose(): void {
+		this.setState({ isModalOpen: false })
+	}
+}
+
+const mapStateToProps = (state: any) => {
+   return {
+      state: {
+         //ToCheck
+         //error: state.calendar.error
+      }
+   }
+}
+
+const mapDispatchToProps = (dispatch: Function) => {
+   return {
+      dispatch: {
+         
+      }
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CalendarViewComponent)
