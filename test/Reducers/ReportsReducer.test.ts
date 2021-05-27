5efa9a84e09a0aa080c5e8fcde7a1d46bad473ac
dkokollari <38751431+dkokollari@users.noreply.github.com>
2021-05-27 16:32:08 +0200

    Added report reducer tests

--- test/Reducers/ReportsReducer.test.ts
+++ test/Reducers/ReportsReducer.test.ts
@@ -1,5 +1,9 @@
 import reportsReducer, { initialState } from "../../src/reducers/reportReducer"
-import { reportTypes } from "../../src/types"
+import {
+	ERROR_REPORT_NOT_FOUND,
+	ERROR_UNKNOWN,
+	reportTypes,
+} from "../../src/types"
 
 describe("reports reducer test", function () {
 	const fakeReports = {
@@ -12,8 +16,8 @@ describe("reports reducer test", function () {
 				},
 				{
 					name: "Registered_Report_usage_20210524_000000.pdf",
-					creationDate: "2021-05-23T00:00:00.028264417",
-					registrationDate: "2021-05-23T00:00:00.028264417",
+					creationDate: "2021-05-24T00:00:00.028264417",
+					registrationDate: "2021-05-24T00:00:00.028264417",
 				},
 			],
 		},
@@ -22,6 +26,72 @@ describe("reports reducer test", function () {
 	it("should have correct initial state", function () {
 		const action = {
 			type: null,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(initialState)
+	})
+
+	it("should correctly create and fetch usage report", function () {
+		const fakeReport = [
+			{
+				"0": "first character of the pdf file",
+			},
+			{
+				"1": "second character of the pdf file",
+			},
+			{
+				"2": "third character of the pdf file",
+			},
+			{
+				"3": "fourth character of the pdf file",
+			},
+		]
+		const action = {
+			type: reportTypes.CREATE_USAGE_SUCCESS,
+			payload: fakeReport,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(initialState)
+	})
+
+	it("should correctly create and fetch cleaner report", function () {
+		const fakeReport = [
+			{
+				"0": "first character of the pdf file",
+			},
+			{
+				"1": "second character of the pdf file",
+			},
+			{
+				"2": "third character of the pdf file",
+			},
+			{
+				"3": "fourth character of the pdf file",
+			},
+		]
+		const action = {
+			type: reportTypes.CREATE_CLEANER_SUCCESS,
+			payload: fakeReport,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(initialState)
+	})
+
+	it("should correctly fetch single report", function () {
+		const fakeReport = [
+			{
+				"0": "first character of the pdf file",
+			},
+			{
+				"1": "second character of the pdf file",
+			},
+			{
+				"2": "third character of the pdf file",
+			},
+			{
+				"3": "fourth character of the pdf file",
+			},
+		]
+		const action = {
+			type: reportTypes.FETCH_SINGLE_SUCCESS,
+			payload: fakeReport,
 		}
 		expect(reportsReducer(initialState, action)).toEqual(initialState)
 	})
@@ -62,6 +132,286 @@ describe("reports reducer test", function () {
 		const state = {
 			reports: undefined,
 			error: "",
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch usage report error 400", function () {
+		const action = {
+			type: reportTypes.CREATE_USAGE_FAILURE,
+			payload: {
+				error: 400,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch usage report error 401", function () {
+		const action = {
+			type: reportTypes.CREATE_USAGE_FAILURE,
+			payload: {
+				error: 401,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch usage report error 404", function () {
+		const action = {
+			type: reportTypes.CREATE_USAGE_FAILURE,
+			payload: {
+				error: 404,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch usage report error 500", function () {
+		const action = {
+			type: reportTypes.CREATE_USAGE_FAILURE,
+			payload: {
+				error: 500,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch usage report error 504", function () {
+		const action = {
+			type: reportTypes.CREATE_USAGE_FAILURE,
+			payload: {
+				error: 504,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch cleaner report error 400", function () {
+		const action = {
+			type: reportTypes.CREATE_CLEANER_FAILURE,
+			payload: {
+				error: 400,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch cleaner report error 401", function () {
+		const action = {
+			type: reportTypes.CREATE_CLEANER_FAILURE,
+			payload: {
+				error: 401,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch cleaner report error 404", function () {
+		const action = {
+			type: reportTypes.CREATE_CLEANER_FAILURE,
+			payload: {
+				error: 404,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch cleaner report error 500", function () {
+		const action = {
+			type: reportTypes.CREATE_CLEANER_FAILURE,
+			payload: {
+				error: 500,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle create and fetch cleaner report error 504", function () {
+		const action = {
+			type: reportTypes.CREATE_CLEANER_FAILURE,
+			payload: {
+				error: 504,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch single report error 400", function () {
+		const action = {
+			type: reportTypes.FETCH_SINGLE_FAILURE,
+			payload: {
+				error: 400,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch single report error 401", function () {
+		const action = {
+			type: reportTypes.FETCH_SINGLE_FAILURE,
+			payload: {
+				error: 401,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch single report error 404", function () {
+		const action = {
+			type: reportTypes.FETCH_SINGLE_FAILURE,
+			payload: {
+				error: 404,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_REPORT_NOT_FOUND,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch single report error 500", function () {
+		const action = {
+			type: reportTypes.FETCH_SINGLE_FAILURE,
+			payload: {
+				error: 500,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch single report error 504", function () {
+		const action = {
+			type: reportTypes.FETCH_SINGLE_FAILURE,
+			payload: {
+				error: 504,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch all reports error 400", function () {
+		const action = {
+			type: reportTypes.FETCH_ALL_FAILURE,
+			payload: {
+				error: 400,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch all reports error 401", function () {
+		const action = {
+			type: reportTypes.FETCH_ALL_FAILURE,
+			payload: {
+				error: 401,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch all reports error 404", function () {
+		const action = {
+			type: reportTypes.FETCH_ALL_FAILURE,
+			payload: {
+				error: 404,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch all reports error 500", function () {
+		const action = {
+			type: reportTypes.FETCH_ALL_FAILURE,
+			payload: {
+				error: 500,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
+		}
+		expect(reportsReducer(initialState, action)).toEqual(state)
+	})
+
+	it("should correctly handle fetch all reports error 504", function () {
+		const action = {
+			type: reportTypes.FETCH_ALL_FAILURE,
+			payload: {
+				error: 504,
+			},
+		}
+		const state = {
+			...initialState,
+			error: ERROR_UNKNOWN,
 		}
 		expect(reportsReducer(initialState, action)).toEqual(state)
 	})
