7b19a7d6616263e69a4d8216f8d54e2dec352738
Michele <michelemotterle97@gmail.com>
2021-06-04 10:55:28 +0200

    Add reservations to rootReducers

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux"
 import loginReducer from "./loginReducer"
 import accountsReducer from "./accountsReducer"
 import roomsReducer from "./roomsReducer"
+import reservationsReducers from "./reservationsReducer"
 
 const rootReducer = combineReducers({
 	login: loginReducer,
 	accounts: accountsReducer,
 	rooms: roomsReducer,
+	reservations: reservationsReducers
 })
 
 export default rootReducer
