f37a30dab79a156710e21073953773c0a90a6bae
Michele <michelemotterle97@gmail.com>
2021-06-04 10:53:05 +0200

    Fixed reservationsAPI

--- src/Api/reservationAPI.ts
+++ src/Api/reservationAPI.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosResponse, AxiosStatic } from "axios"
 
-export class accountAPI {
+export class reservationAPI {
 	private axios: AxiosStatic
 
 	constructor(axios: AxiosStatic) {
 		this.axios = axios
 	}
 
-	calendar(
+	getReservationsByUser(
 		tokenID: string,
 		data: {
 			startTime: string
@@ -20,6 +20,10 @@ export class accountAPI {
 				Authorization: tokenID,
 				"Content-Type": "application/json",
 			},
+			params: {
+				from: data.startTime,
+				to: data.endTime
+			}
 		}
 		return this.axios.get(
 			"/api/reservations/view/user/" + data.username,
@@ -28,4 +32,4 @@ export class accountAPI {
 	}
 }
 
-export default new accountAPI(axios)
+export default new reservationAPI(axios)
