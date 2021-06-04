dc4fa38bf6fe8e08c8c75c2a394a611ff0c487b8
Michele <michelemotterle97@gmail.com>
2021-06-04 10:53:16 +0200

    Update appointments.ts

--- src/appointments.ts
+++ src/appointments.ts
@@ -1,9 +1,9 @@
 export const appointments = [
 	{
 		title: "Stanza Rossa",
-		startDate: new Date(2021, 6, 3, 9, 35),
-		endDate: new Date(2021, 6, 3, 19, 30),
-		id: "60b4af1e7ae20f1cf1805156",
+		startDate: new Date(2021, 5, 3, 9, 35),
+		endDate: new Date(2021, 5, 3, 19, 30),
+		id: "0",
 		location: "Stanza Rossa",
 	},
 ]
